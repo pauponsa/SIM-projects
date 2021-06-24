9ca45248506708b476768444ca13830268b5a7df
pauponsa <80407043+pauponsa@users.noreply.github.com>
2021-06-24 10:52:06 +0200

    Add files via upload

---
+++ ASW projects/aswg13c-frontend-051594ad8496/README.md
@@ -0,0 +1,17 @@
+# README
+
+Equip:
+
+-   Iván Lagunas
+
+-   Aleix Linares
+
+-   Oriol Freixa
+
+-   Agustin Millan
+
+-   Pau Ponsa
+
+Link a l'aplicació desplegada a heroku:
+
+https://hacker-news-but-cool.herokuapp.com/
---
+++ ASW projects/aswg13c-frontend-051594ad8496/src/app/assets/images/index.ts
@@ -0,0 +1,17 @@
+import logo from "./logo.png";
+import speechBubble from "./speech-bubble.png";
+import heart from "./heart.png";
+import heartOutlined from "./heart-outlined.png";
+import filledHeart from "./filled-heart.png"
+import hamburger from "./hamburger.png"
+
+const images = {
+    logo,
+    speechBubble,
+    heart,
+    heartOutlined,
+    filledHeart,
+    hamburger
+};
+
+export default images;
